b32f8276d0ef41393c100af71e96d2cafdbfec28
Yael Buisine <yael.buisine@gmail.com>
2021-12-16 12:55:03 +0200

    Creation of 2 applications (client and admin) and creation of firsts routes

---
+++ apps/eshop/src/app/shared/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'yael-org-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
